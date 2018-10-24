b9bbc044ed25e9ea1a1d32e79795fd81c0223c7b
Viet Asc <vtahdn@gmail.com>
2018-10-24 16:33:25 +0700

    Initial Commit

---
+++ LinhKa_Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinhKa_Alert
+//
+//  Created by Viet Asc on 10/24/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LinhKa_AlertTests/LinhKa_AlertTests.swift
@@ -0,0 +1,34 @@
+//
+//  LinhKa_AlertTests.swift
+//  LinhKa_AlertTests
+//
+//  Created by Viet Asc on 10/24/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import LinhKa_Alert
+
+class LinhKa_AlertTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
